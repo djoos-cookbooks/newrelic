6c71cbb1aa66c29e2f5ee4a0925953d25f4c0fdd
David Joos <david.joos@gmail.com>
2012-05-21 17:41:41 +0100

    Supported OS

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,5 @@ license          "MIT"
 description      "Installs/Configures New Relic"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+supports "ubuntu", "10.04"
