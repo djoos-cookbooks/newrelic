c63102ee9c49e8158f91823e08efcf87d34fd633
David Joos <david.joos@gmail.com>
2012-11-27 09:57:05 +0000

    Add support for scientific linux

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,14 @@ maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures New Relic"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.7"
+version          "0.3.8"
 
 supports "ubuntu"
 supports "debian"
 supports "centos"
 supports "redhat"
 supports "fedora"
+supports "scientific"
 
 depends "apache2"
 depends "php"
