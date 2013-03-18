e308563c5f3f4d16281d7d321b4f2ac29c23366c
David Joos <david.joos@gmail.com>
2013-03-18 11:44:54 +0000

    splitting up attributes into recipe-specific files

---
+++ attributes/php-agent.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: newrelic
+# Attributes:: php-agent
+#
+# Copyright 2012-2013, Escape Studios
+#
+
+default['newrelic']['startup_mode'] = "agent"
+default['newrelic']['web_server']['service_name'] = "apache2"
---
+++ attributes/python-agent.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: newrelic
+# Attributes:: python-agent
+#
+# Copyright 2012-2013, Escape Studios
+#
+
+default['newrelic']['python_version'] = "latest"
