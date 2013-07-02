6dac7018b761bbe9e8698cd5a8bef36622c945e3
David Joos <david.joos@gmail.com>
2013-07-02 23:26:17 +0100

    Additional tweaks to Alex' changes, cleanup

--- attributes/server-monitor.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: newrelic
-# Attributes:: server-monitor
-#
-# Copyright 2013, Medidata Solutions
-#
-default['newrelic']['server_monitoring']['windows_version'] = "2.0.0.198"
-default['newrelic']['server_monitoring']['windows64_checksum'] = "5a8f3f5e8f15997463430401756d377c321c8899c2790ca85e5587a5b643651e"
-default['newrelic']['server_monitoring']['windows32_checksum'] = "ac2b65eecaad461fdd2e4386e3e4c9f96ea940b35bdf7a8c532c21dbd1c99ff0"
--- recipes/install.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: newrelic
-# Recipe:: install
-#
-# Copyright 2012-2013, Escape Studios
-#
-
-include_recipe "newrelic::repository"
-include_recipe "newrelic::server-monitor"
