72130286fb4ab1a22cc2ac36d83421a549490fe7
Marvin Marcelo <marvin@Marvins-MacBook-Pro.local>
2013-07-16 16:01:24 +0800

    Define resource before using it

--- recipes/install.rb
+++ recipes/install.rb
@@ -14,6 +14,11 @@ case node[:platform]
         gpg_key_id = node[:newrelic][:repository_key]
         gpg_key_url = "http://download.newrelic.com/#{gpg_key_id}.gpg"
 
+        execute "newrelic-apt-get-update" do
+            command "apt-get update"
+            action :nothing
+        end
+
         execute "newrelic-add-gpg-key" do
             command "wget -O - #{gpg_key_url} | apt-key add -"
             notifies :run, "execute[newrelic-apt-get-update]", :immediately
@@ -28,12 +33,6 @@ case node[:platform]
             mode 0644
             notifies :run, "execute[newrelic-apt-get-update]", :immediately
             action :create_if_missing
-        end
-
-        #update the local package list
-        execute "newrelic-apt-get-update" do
-            command "apt-get update"
-            action :nothing
         end
     when "redhat", "centos", "fedora", "scientific", "amazon"
         #install the newrelic-repo package, which configures a new package repository for yum
