ecf3bcf164282e8bd035da77214e2464867ac655
David Joos <david.joos@gmail.com>
2013-08-15 21:29:03 +0100

    Added .NET Agent install, version bump fixes #26

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures New Relic"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.0"
+version          "0.5.1"
 
 %w{ debian ubuntu redhat centos fedora scientific amazon windows }.each do |os|
 supports os
