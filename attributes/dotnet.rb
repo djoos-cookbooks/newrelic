6e526dcf444e1ead982e616a1493e80726d0d0de
David Joos <david.joos@gmail.com>
2013-08-15 21:27:17 +0100

    Cleanup

--- attributes/dotnet.rb
+++ attributes/dotnet.rb
@@ -1,3 +1,9 @@
+#
+# Cookbook Name:: newrelic
+# Attributes:: dotnet
+#
+# Copyright 2012-2013, Escape Studios
+#
 
 default['newrelic']['dotnet']['https_download'] = 'https://download.newrelic.com/dot_net_agent/release/x64'
 default['newrelic']['dotnet']['install_level'] = '1'
--- recipes/dotnet.rb
+++ recipes/dotnet.rb
@@ -8,9 +8,9 @@
 include_recipe "ms_dotnet4"
 
 windows_package "Install New Relic .NET Agent" do
-	source node['newrelic']['dotnet']['https_download']
-	options "/qb NR_LICENSE_KEY=#{node['newrelic']['application_monitoring']['license']} INSTALLLEVEL=#{node['newrelic']['dotnet']['install_level']}"
-	installer_type :msi
-	action :install
-	not_if{File.exists?("C:\\Program Files\\New Relic\\.NET Agent")}
+    source node['newrelic']['dotnet']['https_download']
+    options "/qb NR_LICENSE_KEY=#{node['newrelic']['application_monitoring']['license']} INSTALLLEVEL=#{node['newrelic']['dotnet']['install_level']}"
+    installer_type :msi
+    action :install
+    not_if{File.exists?("C:\\Program Files\\New Relic\\.NET Agent")}
 end
