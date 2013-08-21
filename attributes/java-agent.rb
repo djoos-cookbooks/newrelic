2ec7781eaf0d3699c7f86a25a4f99b9d0db63c72
Steven Gonzales <stevendgonzales@gmail.com>
2013-08-21 07:26:54 -0500

    add java agent attributes, recipe, and template

---
+++ attributes/java-agent.rb
@@ -0,0 +1,7 @@
+default['newrelic']['java']['https_download_url'] = 'https://download.newrelic.com/newrelic/java-agent/newrelic-agent/2.21.3/newrelic-agent-2.21.3.jar'
+default['newrelic']['java']['jar_file'] = 'newrelic-agent-2.21.3.jar'
+default['newrelic']['java']['install_dir'] = ''
+default['newrelic']['java']['audit_mode'] = false
+default['newrelic']['java']['log_file_count'] = 1
+default['newrelic']['java']['log_limit_in_kbytes'] = 0
+default['newrelic']['java']['log_daily'] = false
---
+++ recipes/java-agent.rb
@@ -0,0 +1,42 @@
+
+#Create the directory to install the jar into
+directory node['newrelic']['java']['install_dir'] do
+  action :create
+end
+
+local_file = node['newrelic']['java']['install_dir'] + '/newrelic.jar'
+remote_file local_file do
+  source   node['newrelic']['java']['https_download_url']
+  mode 0644
+end
+
+#configure your New Relic license key
+conf_file = node['newrelic']['java']['install_dir'] + '/newrelic.yml'
+template conf_file do
+    source "newrelic.yml.java.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+        :license => node['newrelic']['application_monitoring']['license'],
+        :appname => node['newrelic']['application_monitoring']['appname'],
+        :logfile => node['newrelic']['application_monitoring']['logfile'],
+        :loglevel => node['newrelic']['application_monitoring']['loglevel'],
+        :audit_mode => node['newrelic']['java']['audit_mode'], 
+        :log_file_count => node['newrelic']['java']['log_file_count'],
+        :log_limit_in_kbytes => node['newrelic']['java']['log_limit_in_kbytes'],
+        :log_daily => node['newrelic']['java']['log_daily'], 
+        :daemon_ssl => node['newrelic']['application_monitoring']['daemon']['ssl'],        
+        :capture_params => node['newrelic']['application_monitoring']['capture_params'],
+        :ignored_params => node['newrelic']['application_monitoring']['ignored_params'],
+        :transaction_tracer_enable => node['newrelic']['application_monitoring']['transaction_tracer']['enable'],
+        :transaction_tracer_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['threshold'],
+        :transaction_tracer_record_sql => node['newrelic']['application_monitoring']['transaction_tracer']['record_sql'],
+        :transaction_tracer_stack_trace_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['stack_trace_threshold'],
+        :transaction_tracer_slow_sql => node['newrelic']['application_monitoring']['transaction_tracer']['slow_sql'],
+        :transaction_tracer_explain_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['explain_threshold'],
+        :error_collector_enable => node['newrelic']['application_monitoring']['error_collector']['enable'],
+        :browser_monitoring_auto_instrument => node['newrelic']['application_monitoring']['browser_monitoring']['auto_instrument']
+    )
+    action :create
+end
