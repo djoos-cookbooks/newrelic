0251d78dd27cdbd448a860f3d85dc63efab1d232
Jeff Byrnes <jeff@evertrue.com>
2013-09-05 10:24:47 -0400

    Add header to plugin-agent attributes to correctly specify license

--- attributes/plugin-agent.rb
+++ attributes/plugin-agent.rb
@@ -1,3 +1,22 @@
+#
+# Cookbook Name:: newrelic
+# Attributes:: plugin-agent
+#
+# Copyright 2012, Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 # plugin-agent
 default['newrelic']['plugin-agent']['poll_interval'] = 60
 default['newrelic']['plugin-agent']['pidfile'] = "/var/run/newrelic/newrelic_plugin_agent.pid"
