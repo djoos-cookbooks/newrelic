ba419c2b6c9d6233ab5f067152491b5124d72833
atrullmdsol <atrull@mdsol.com>
2013-09-20 16:26:12 +0200

    Reverted separate attributes - service_name is used both to refer to the service and the package, this with the service name not being needed on windows, makes the most sense.

--- attributes/server-monitor.rb
+++ attributes/server-monitor.rb
@@ -5,11 +5,6 @@
 # Copyright 2012-2013, Escape Studios
 #
 
-case node['platform']
-when "windows"
-  default['newrelic']['service_name'] = "NewRelic.ServerMonitor.Service"
-else
-  default['newrelic']['service_name'] = "newrelic-sysmond"
-  default['newrelic']['config_path'] = "/etc/newrelic"
-  default['newrelic']['config_file_group'] = "newrelic"
-end
+default['newrelic']['service_name'] = "newrelic-sysmond"
+default['newrelic']['config_path'] = "/etc/newrelic"
+default['newrelic']['config_file_group'] = "newrelic"
