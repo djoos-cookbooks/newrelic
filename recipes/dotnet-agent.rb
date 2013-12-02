28845c4418675beba0e883531fcc90d51708c5a9
Raymond L. Colanero <rcolanero@mdsol.com>
2013-12-02 12:06:33 -0500

    removed check for .NET pre-existing in Windows installs

--- recipes/dotnet-agent.rb
+++ recipes/dotnet-agent.rb
@@ -5,9 +5,7 @@
 # Copyright 2012-2013, Escape Studios
 #
 
-if !File.exists?("C:/Windows/Microsoft.NET/Framework/v4.0.30319")
 	include_recipe "ms_dotnet4"
-end
 
 windows_package "Install New Relic .NET Agent" do
     source node['newrelic']['dotnet-agent']['https_download']
