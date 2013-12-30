104c502b77cc103f99d144a942bd8fe7a7354f75
Benoit Caron <bcaron@lapresse.ca>
2013-12-30 13:54:49 -0500

    Adding support for Suse - server monitor

--- attributes/server-monitor.rb
+++ attributes/server-monitor.rb
@@ -8,3 +8,7 @@
 default['newrelic']['service_name'] = "newrelic-sysmond"
 default['newrelic']['config_path'] = "/etc/newrelic"
 default['newrelic']['config_file_group'] = "newrelic"
+
+# targz install  method
+default['newrelic']['sysmond_tar_version'] = "1.3.1.437"
+default['newrelic']['sysmond_tar_baseurl'] = "https://download.newrelic.com/server_monitor/release/newrelic-sysmond"
