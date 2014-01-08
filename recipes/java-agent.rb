59bf46e65cd0b54758ca0dcfca078ade95410ce0
Benoit Caron <bcaron@lapresse.ca>
2014-01-08 15:38:30 -0500

    Replace the template by the newrelic_yml LWRP

--- recipes/java-agent.rb
+++ recipes/java-agent.rb
@@ -24,35 +24,32 @@ end
 
 if node['newrelic']['application_monitoring']['appname'].nil?
     node.set['newrelic']['application_monitoring']['appname'] = node['hostname']
-end   
+end
 
 #configure your New Relic license key
 conf_file = node['newrelic']['install_dir'] + '/newrelic.yml'
-template conf_file do
-    source "newrelic.yml.java.erb"
-    owner node['newrelic']['app_user']
-    group node['newrelic']['app_group']
-    mode 0644
-    variables(
-        :license => node['newrelic']['application_monitoring']['license'],
-        :appname => node['newrelic']['application_monitoring']['appname'],
-        :logfile => node['newrelic']['application_monitoring']['logfile'],
-        :loglevel => node['newrelic']['application_monitoring']['loglevel'],
-        :audit_mode => node['newrelic']['audit_mode'], 
-        :log_file_count => node['newrelic']['log_file_count'],
-        :log_limit_in_kbytes => node['newrelic']['log_limit_in_kbytes'],
-        :log_daily => node['newrelic']['log_daily'], 
-        :daemon_ssl => node['newrelic']['application_monitoring']['daemon']['ssl'],        
-        :capture_params => node['newrelic']['application_monitoring']['capture_params'],
-        :ignored_params => node['newrelic']['application_monitoring']['ignored_params'],
-        :transaction_tracer_enable => node['newrelic']['application_monitoring']['transaction_tracer']['enable'],
-        :transaction_tracer_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['threshold'],
-        :transaction_tracer_record_sql => node['newrelic']['application_monitoring']['transaction_tracer']['record_sql'],
-        :transaction_tracer_stack_trace_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['stack_trace_threshold'],
-        :transaction_tracer_slow_sql => node['newrelic']['application_monitoring']['transaction_tracer']['slow_sql'],
-        :transaction_tracer_explain_threshold => node['newrelic']['application_monitoring']['transaction_tracer']['explain_threshold'],
-        :error_collector_enable => node['newrelic']['application_monitoring']['error_collector']['enable'],
-        :browser_monitoring_auto_instrument => node['newrelic']['application_monitoring']['browser_monitoring']['auto_instrument']
-    )
-    action :create
+newrelic_yml conf_file do
+  agent_type "java"
+  app_name  node['newrelic']['application_monitoring']['appname']
+  owner node['newrelic']['app_user']
+  group node['newrelic']['app_group']
+
+  license  node['newrelic']['application_monitoring']['license']
+  logfile  node['newrelic']['application_monitoring']['logfile']
+  loglevel  node['newrelic']['application_monitoring']['loglevel']
+  audit_mode  node['newrelic']['audit_mode']
+  log_file_count  node['newrelic']['log_file_count']
+  log_limit_in_kbytes  node['newrelic']['log_limit_in_kbytes']
+  log_daily  node['newrelic']['log_daily']
+  daemon_ssl  node['newrelic']['application_monitoring']['daemon']['ssl']
+  capture_params  node['newrelic']['application_monitoring']['capture_params']
+  ignored_params  node['newrelic']['application_monitoring']['ignored_params']
+  transaction_tracer_enable  node['newrelic']['application_monitoring']['transaction_tracer']['enable']
+  transaction_tracer_threshold  node['newrelic']['application_monitoring']['transaction_tracer']['threshold']
+  transaction_tracer_record_sql  node['newrelic']['application_monitoring']['transaction_tracer']['record_sql']
+  transaction_tracer_stack_trace_threshold  node['newrelic']['application_monitoring']['transaction_tracer']['stack_trace_threshold']
+  transaction_tracer_slow_sql  node['newrelic']['application_monitoring']['transaction_tracer']['slow_sql']
+  transaction_tracer_explain_threshold  node['newrelic']['application_monitoring']['transaction_tracer']['explain_threshold']
+  error_collector_enable  node['newrelic']['application_monitoring']['error_collector']['enable']
+  browser_monitoring_auto_instrument  node['newrelic']['application_monitoring']['browser_monitoring']['auto_instrument']
 end
