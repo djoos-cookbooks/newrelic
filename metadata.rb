63abf7b06b07beef746bd2aa87cd690cfc4a6422
David Joos <david.joos@gmail.com>
2014-03-07 11:51:15 -0300

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures New Relic"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.8"
+version          "0.6.0"
 
 %w{ debian ubuntu redhat centos fedora scientific amazon windows smartos }.each do |os|
 supports os
@@ -21,9 +21,10 @@ recommends "nodejs"
 recipe "newrelic", "Adds the New Relic repository, installs & configures the New Relic server monitor package."
 recipe "newrelic::repository", "Adds the New Relic repository."
 recipe "newrelic::server-monitor", "Installs & configures the New Relic server monitor package."
+recipe "newrelic::dotnet-agent", "Installs New Relic .NET Agent"
+recipe "newrelic::java-agent", "Installs the New Relic Java agent."
+recipe "newrelic::nodejs-agent", "Installs New Relic Node.js Agent"
 recipe "newrelic::php-agent", "Installs the New Relic PHP agent."
 recipe "newrelic::python-agent", "Installs the New Relic Python agent."
-recipe "newrelic::dotnet", "Installs New Relic .NET Agent"
-recipe "newrelic::java-agent", "Installs the New Relic Java agent."
-recipe "newrelic::nodejs", "Installs New Relic Node.js Agent"
-recipe "newrelic::plugin-agent", "Installs the New Relic plugin agent."
+recipe "newrelic::ruby-agent", "Installs the New Relic Ruby agent."
+recipe "newrelic::meetme-plugin-agent", "Installs the New Relic MeetMe plugin agent."
