5ce41446050f9b18e36d0e7615d63ddbee1f0c17
David Joos <david.joos@gmail.com>
2014-03-21 20:37:29 +0000

    Add virtualenv support for the python agent, fixes #65

--- attributes/python-agent.rb
+++ attributes/python-agent.rb
@@ -7,4 +7,5 @@
 
 default['newrelic']['python-agent']['python_version'] = "latest"
 default['newrelic']['python-agent']['python_recipe'] = "python::pip"
+default['newrelic']['python-agent']['python_venv'] = nil
 default['newrelic']['python-agent']['config_file'] = "/etc/newrelic/newrelic.ini"
