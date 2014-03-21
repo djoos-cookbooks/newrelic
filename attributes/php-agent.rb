ee4552642ca48bec6543a505de85d97b4179459c
David Joos <david.joos@gmail.com>
2014-03-21 20:28:56 +0000

    Inspired by #67, an update to the original fix, fixes #56

--- attributes/php-agent.rb
+++ attributes/php-agent.rb
@@ -8,4 +8,4 @@
 default['newrelic']['php-agent']['startup_mode'] = "agent"
 default['newrelic']['php-agent']['web_server']['service_name'] = "apache2"
 default['newrelic']['php-agent']['php_recipe'] = "php::default"
-default['newrelic']['php-agent']['php']['ext_conf_dir'] = node['php']['ext_conf_dir']
+default['newrelic']['php-agent']['config_file'] = "#{node['php']['ext_conf_dir']}/newrelic.ini"
