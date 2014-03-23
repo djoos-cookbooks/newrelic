aae17ad5e852bf9b25e6a50562304c1851d4b688
David Joos <david.joos@gmail.com>
2014-03-23 23:58:50 +0000

    Missed EOY update(s)

--- recipes/meetme-plugin.rb
+++ recipes/meetme-plugin.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: newrelic
 # Recipe:: meetme-plugin
 #
-# Copyright 2012-2013, Escape Studios
+# Copyright 2012-2014, Escape Studios
 #
 
 include_recipe node['newrelic']['meetme-plugin']['python_recipe']
