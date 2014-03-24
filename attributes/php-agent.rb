79645e86a0abad8329ad543edf88ddd9fd961745
David Joos <david.joos@gmail.com>
2014-03-24 00:35:26 +0000

    Don't crash in case the PHP cookbook can't be found

--- attributes/php-agent.rb
+++ attributes/php-agent.rb
@@ -5,7 +5,15 @@
 # Copyright 2012-2014, Escape Studios
 #
 
+php_ext_conf_dir = nil
+
+if node.attribute?('php')
+	if node.attribute?('ext_conf_dir')
+		php_ext_conf_dir = node['php']['ext_conf_dir']
+  	end
+end
+
 default['newrelic']['php-agent']['startup_mode'] = "agent"
 default['newrelic']['php-agent']['web_server']['service_name'] = "apache2"
 default['newrelic']['php-agent']['php_recipe'] = "php::default"
-default['newrelic']['php-agent']['config_file'] = "#{node['php']['ext_conf_dir']}/newrelic.ini"
+default['newrelic']['php-agent']['config_file'] = "#{php_ext_conf_dir}/newrelic.ini"
