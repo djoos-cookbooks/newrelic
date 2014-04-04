8cd7b4b08a49a1e9dd5c9baa06808e3c60e1563d
David Joos <david.joos@gmail.com>
2014-04-05 00:33:50 +0100

    Refactoring

--- recipes/repository.rb
+++ recipes/repository.rb
@@ -14,21 +14,16 @@ case node['platform']
 when 'debian', 'ubuntu'
   # trust the New Relic GPG Key
   # this step is required to tell apt that you trust the integrity of New Relic's apt repository
-  gpg_key_id = node['newrelic']['repository']['repository_key']
-  gpg_key_url = "http://download.newrelic.com/#{gpg_key_id}.gpg"
-  gpg_key_file = "#{Chef::Config['file_cache_path']}/newrelic-gpg-key"
+  gpg_key_url = "http://download.newrelic.com/#{node['newrelic']['repository']['repository_key']}.gpg"
+  gpg_key_file = "#{Chef::Config['file_cache_path']}/#{node['newrelic']['repository']['repository_key']}.gpg"
 
   remote_file gpg_key_file do
     source gpg_key_url
-    notifies :run, 'execute[newrelic-add-apt-key]', :immediately
-    action :create_if_missing
-    not_if "apt-key list | grep #{gpg_key_id}"
+    action :create
   end
 
   execute 'newrelic-add-apt-key' do
     command "apt-key add #{gpg_key_file}"
-    action :nothing
-    notifies :run, 'execute[newrelic-apt-get-update]', :immediately
   end
 
   # configure the New Relic apt repository
@@ -41,7 +36,6 @@ when 'debian', 'ubuntu'
     action :create_if_missing
   end
 
-  # update the local package list
   execute 'newrelic-apt-get-update' do
     command 'apt-get update'
     action :nothing
@@ -54,13 +48,13 @@ when 'redhat', 'centos', 'fedora', 'scientific', 'amazon'
     machine = 'i386'
   end
 
-  remote_file "#{Chef::Config['file_cache_path'] || '/tmp'}/newrelic-repo-5-3.noarch.rpm" do
+  remote_file "#{Chef::Config['file_cache_path']}/newrelic-repo-5-3.noarch.rpm" do
     source "http://download.newrelic.com/pub/newrelic/el5/#{machine}/newrelic-repo-5-3.noarch.rpm"
     action :create_if_missing
   end
 
   package 'newrelic-repo' do
-    source "#{Chef::Config['file_cache_path'] || '/tmp'}/newrelic-repo-5-3.noarch.rpm"
+    source "#{Chef::Config['file_cache_path']}/newrelic-repo-5-3.noarch.rpm"
     provider Chef::Provider::Package::Rpm
     action :install
   end
