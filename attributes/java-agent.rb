6b8de29e0f4341bf94a64fbf018581de0db27252
Ramon Makkelie <ramonmakkelie@gmail.com>
2014-07-29 13:02:24 +0200

    Update java-agent.rb

--- attributes/java-agent.rb
+++ attributes/java-agent.rb
@@ -5,9 +5,9 @@
 # Copyright 2012-2014, Escape Studios
 #
 
-default['newrelic']['java-agent']['version'] = 3.8.2
+default['newrelic']['java-agent']['version'] = '3.8.2'
 default['newrelic']['java-agent']['https_download'] = "https://download.newrelic.com/newrelic/java-agent/newrelic-agent/#{node['newrelic']['java-agent']['version']}/newrelic-agent-#{node['newrelic']['java-agent']['version']}.jar"
-default['newrelic']['java-agent']['jar_file'] = 'newrelic-agent-#{node['newrelic']['java-agent']['version']}.jar'
+default['newrelic']['java-agent']['jar_file'] = "newrelic-agent-#{node['newrelic']['java-agent']['version']}.jar"
 default['newrelic']['java-agent']['install_dir'] = '/opt/newrelic/java'
 default['newrelic']['java-agent']['app_user'] = 'newrelic'
 default['newrelic']['java-agent']['app_group'] = 'newrelic'
