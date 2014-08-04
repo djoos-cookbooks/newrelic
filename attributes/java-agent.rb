1bb6ded199f9f030b7444d7f8977060b39026569
Ramon Makkelie <ramonmakkelie@gmail.com>
2014-08-04 09:56:34 +0200

    bump version

--- attributes/java-agent.rb
+++ attributes/java-agent.rb
@@ -5,7 +5,7 @@
 # Copyright 2012-2014, Escape Studios
 #
 
-default['newrelic']['java-agent']['version'] = '3.8.2'
+default['newrelic']['java-agent']['version'] = '3.9.0'
 default['newrelic']['java-agent']['https_download'] = "https://download.newrelic.com/newrelic/java-agent/newrelic-agent/#{node['newrelic']['java-agent']['version']}/newrelic-agent-#{node['newrelic']['java-agent']['version']}.jar"
 default['newrelic']['java-agent']['jar_file'] = "newrelic-agent-#{node['newrelic']['java-agent']['version']}.jar"
 default['newrelic']['java-agent']['install_dir'] = '/opt/newrelic/java'
