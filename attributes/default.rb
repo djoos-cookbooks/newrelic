a466e76713f4153fcf7463fdb4ce249c8f0af973
David Joos <david.joos@gmail.com>
2014-10-24 16:51:44 +0100

    'combine' proxy settings

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,13 +13,17 @@ default['newrelic']['license'] = nil
 default['newrelic']['server_monitoring']['license'] = node['newrelic']['license']
 default['newrelic']['application_monitoring']['license'] = node['newrelic']['license']
 
+# proxy
+default['newrelic']['proxy'] = nil
+default['newrelic']['server_monitoring']['proxy'] = node['newrelic']['proxy']
+default['newrelic']['application_monitoring']['daemon']['proxy'] = node['newrelic']['proxy']
+
 #################
 # ADVANCED CONFIG
 #################
 # server monitoring
 default['newrelic']['server_monitoring']['logfile'] = nil
 default['newrelic']['server_monitoring']['loglevel'] = nil
-default['newrelic']['server_monitoring']['proxy'] = nil
 default['newrelic']['server_monitoring']['ssl'] = nil
 default['newrelic']['server_monitoring']['ssl_ca_bundle'] = nil
 default['newrelic']['server_monitoring']['ssl_ca_path'] = nil
@@ -41,7 +45,6 @@ default['newrelic']['application_monitoring']['daemon']['max_threads'] = nil
 default['newrelic']['application_monitoring']['daemon']['ssl'] = nil
 default['newrelic']['application_monitoring']['daemon']['ssl_ca_path'] = nil
 default['newrelic']['application_monitoring']['daemon']['ssl_ca_bundle'] = nil
-default['newrelic']['application_monitoring']['daemon']['proxy'] = nil
 default['newrelic']['application_monitoring']['daemon']['pidfile'] = nil
 default['newrelic']['application_monitoring']['daemon']['location'] = nil
 default['newrelic']['application_monitoring']['daemon']['collector_host'] = nil
