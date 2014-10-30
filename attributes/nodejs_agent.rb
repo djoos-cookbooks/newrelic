1de381ed9572c9702b0d0719b7321b35e6b91573
Eric Tucker <eric.tucker@bluespurs.com>
2014-10-30 11:43:32 -0300

    Adding support for specifying the log level and filepath for nodejs agent log file.

--- attributes/nodejs_agent.rb
+++ attributes/nodejs_agent.rb
@@ -9,3 +9,5 @@ default['newrelic']['nodejs_agent']['agent_action'] = :install
 default['newrelic']['nodejs_agent']['apps'] = []
 default['newrelic']['nodejs_agent']['template']['cookbook'] = 'newrelic'
 default['newrelic']['nodejs_agent']['template']['source'] = 'agent/nodejs/newrelic.js.erb'
+
+default['newrelic']['nodejs_agent']['default_app_log_level'] = "INFO"
