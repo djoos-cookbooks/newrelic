88a81963cc57cc1ddb8d78862ac7c4b3f4e7a528
Nielsen Pierce <nielsen.pierce@rackspace.co.uk>
2015-04-09 11:31:56 +0100

    add unit tests and update spec_helper

---
+++ test/spec/unit/server_monitor_spec.rb
@@ -0,0 +1,42 @@
+require_relative 'spec_helper'
+
+describe 'newrelic_lwrp_test::server_monitor' do
+  before do
+    stub_resources
+  end
+
+  ::CHECK_OPTS = {
+    :log_level => LOG_LEVEL,
+    :platform => 'centos',
+    :version => '6.5',
+    :step_into => ['newrelic_server_monitor']
+  }
+
+  context 'Any OS' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(CHECK_OPTS) do |node|
+        stub_node_resources(node)
+      end.converge(described_recipe)
+    end
+
+    it 'Installs Newrelic PHP agent' do
+      expect(chef_run).to install_newrelic_server_monitor('Install')
+    end
+
+    it 'creates a yum_repository for newrelic' do
+      expect(chef_run).to create_yum_repository('newrelic')
+    end
+
+    it 'installs nrsysmond' do
+      expect(chef_run).to install_package('newrelic-sysmond')
+    end
+
+    it 'creates newrelic config template from nrsysmond.cfg.erb' do
+      expect(chef_run).to render_file('/etc/newrelic/nrsysmond.cfg')
+    end
+
+    it 'enables service' do
+      expect(chef_run).to_not enable_service('newrelic-sysmond')
+    end
+  end
+end
