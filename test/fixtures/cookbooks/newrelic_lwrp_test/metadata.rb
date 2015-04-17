06d44b425731f3426b425fa9184710708cb1648a
Nielsen Pierce <nielsen_pierce@hotmail.com>
2015-04-17 15:23:36 +0100

    Update metadata.rb

--- test/fixtures/cookbooks/newrelic_lwrp_test/metadata.rb
+++ test/fixtures/cookbooks/newrelic_lwrp_test/metadata.rb
@@ -1,8 +1,8 @@
 # Encoding: utf-8
 name 'newrelic_lwrp_test'
-maintainer 'Rackspace'
-maintainer_email 'none'
-license ''
+maintainer 'Escape Studios'
+maintainer_email 'dev@escapestudios.com'
+license 'MIT'
 description 'Installs/Configures newrelic_poc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
