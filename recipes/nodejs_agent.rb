441c3399a59b283b4ff648f8173a13e2069de4ad
Cameron Boulton <cboulton@fluid.com>
2015-06-11 13:27:00 -0700

    Expanded NodeJS LWRP and template to support 'capture_params', 'ignored_params' agent configuration options.

--- recipes/nodejs_agent.rb
+++ recipes/nodejs_agent.rb
@@ -13,5 +13,7 @@ node['newrelic']['nodejs_agent']['apps'].each do |nodeapp|
     app_name nodeapp['app_name'] unless nodeapp['app_name'].nil?
     app_log_level nodeapp['app_log_level'] unless nodeapp['app_log_level'].nil?
     app_log_filepath nodeapp['app_log_filepath'] unless nodeapp['app_log_filepath'].nil?
+    capture_params nodeapp['capture_params'] unless nodeapp['capture_params'].nil?
+    ignored_params nodeapp['ignored_params'] unless nodeapp['ignored_params'].nil?
   end
 end
