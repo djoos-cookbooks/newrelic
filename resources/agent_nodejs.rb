4971a1625662eb00e1c332336eb7bb7519071e12
Sonu Kr. Meena <sonukr.meena@practo.com>
2015-12-01 23:43:42 +0530

    fix chef-13 compatibility issues

--- resources/agent_nodejs.rb
+++ resources/agent_nodejs.rb
@@ -12,7 +12,7 @@ default_action :install
 attribute :license, :kind_of => String, :required => true, :default => NewRelic.application_monitoring_license(node)
 attribute :version, :kind_of => String, :default => nil
 attribute :app_name, :kind_of => String, :default => 'My Node App'
-attribute :app_path, :kind_of => String, :name_attribute => true, :required => true, :default => nil
+attribute :app_path, :kind_of => String, :name_attribute => true, :required => true
 attribute :cookbook, :kind_of => String, :default => 'newrelic'
 attribute :source, :kind_of => String, :default => 'agent/nodejs/newrelic.js.erb'
 attribute :enabled, :kind_of => [TrueClass, FalseClass], :default => true
