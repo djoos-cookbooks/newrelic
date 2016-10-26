810ba258dbe553549fb15a4b15427a79b7201ea6
David Joos <david.joos@gmail.com>
2016-10-26 17:51:39 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'development@davidjoos.com'
 license 'MIT'
 description 'Installs/Configures New Relic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.22.3'
+version '2.22.4'
 
 %w(debian ubuntu redhat centos fedora scientific amazon windows smartos oracle).each do |os|
   supports os
