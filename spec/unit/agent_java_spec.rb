542dd5dc7dffd17f56c073ba8235c81e122ea2ba
Rene Martin <rene_martin@intuit.com>
2016-12-20 17:07:56 +0000

    Fix rubocop violation

--- spec/unit/agent_java_spec.rb
+++ spec/unit/agent_java_spec.rb
@@ -46,7 +46,7 @@ describe 'newrelic_lwrp_test::agent_java' do
       ChefSpec::Runner.new(:log_level => LOG_LEVEL, :platform => 'centos', :version => '6.6', :step_into => ['newrelic_agent_java']) do |node|
         stub_node_resources(node)
         node.set['newrelic']['java_agent']['class_transformer_config'] = {
-          'classloader_blacklist' => ['class1', 'class2'],
+          'classloader_blacklist' => %w(class1 class2),
           'instrumentation_classes' => {
             'wildfly-8' => { 'enabled' => false },
             'wildfly-8-CAT' => { 'enabled' => false },
