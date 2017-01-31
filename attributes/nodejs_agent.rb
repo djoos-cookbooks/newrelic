30215a6d9f2b33ade0b032663cb6e082508a5e5e
David Joos <david.joos@gmail.com>
2017-01-31 09:47:09 +0000

    Default agent_actions converted to string, fixes #286

--- attributes/nodejs_agent.rb
+++ attributes/nodejs_agent.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2016, David Joos
 #
 
-default['newrelic']['nodejs_agent']['agent_action'] = :install
+default['newrelic']['nodejs_agent']['agent_action'] = 'install'
 default['newrelic']['nodejs_agent']['apps'] = []
 default['newrelic']['nodejs_agent']['template']['cookbook'] = 'newrelic'
 default['newrelic']['nodejs_agent']['template']['source'] = 'agent/nodejs/newrelic.js.erb'
