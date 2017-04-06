161d97c1ba8ec1cc62956029022f2af496ff19c2
David Joos <david.joos@gmail.com>
2017-04-06 15:07:59 +0100

    Ability to specify New Relic NodeJS agent version

--- resources/agent_nodejs.rb
+++ resources/agent_nodejs.rb
@@ -10,7 +10,7 @@ actions :install, :remove
 default_action :install
 
 attribute :license, :kind_of => String, :required => true, :default => NewRelic.application_monitoring_license(node)
-attribute :version, :kind_of => String, :default => nil
+attribute :version, :kind_of => String, :default => 'latest'
 attribute :app_name, :kind_of => String, :default => 'My Node App'
 attribute :app_path, :kind_of => String, :name_attribute => true, :required => true
 attribute :cookbook, :kind_of => String, :default => 'newrelic'
