4d3f81da6e6c4ad21c975f539cee9d9ef740fd8c
David Joos <david.joos@gmail.com>
2017-04-06 15:09:18 +0100

    Rename test fixture cookbook: newrelic_poc > newrelic_lwrp_test

--- test/fixtures/cookbooks/newrelic_lwrp_test/recipes/agent_java.rb
+++ test/fixtures/cookbooks/newrelic_lwrp_test/recipes/agent_java.rb
@@ -1,6 +1,6 @@
 # Encoding: utf-8
 #
-# Cookbook Name:: newrelic_poc
+# Cookbook Name:: newrelic_lwrp_test
 # Recipe:: agent_java
 #
 # Copyright (c) 2016, David Joos
--- test/fixtures/cookbooks/newrelic_lwrp_test/recipes/server_monitor.rb
+++ test/fixtures/cookbooks/newrelic_lwrp_test/recipes/server_monitor.rb
@@ -1,6 +1,6 @@
 # Encoding: utf-8
 #
-# Cookbook Name:: newrelic_poc
+# Cookbook Name:: newrelic_lwrp_test
 # Recipe:: server_monitor
 #
 # Copyright (c) 2016, David Joos
