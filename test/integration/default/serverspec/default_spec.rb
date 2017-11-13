89354c21a8034bbf3767226ae908ca4f5ff0724b
Bill Ruddock <bill.ruddock@gmail.com>
2017-11-13 19:39:15 +0000

    Allow use of resources with blank default recipe

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper'
 
 describe 'newrelic::default' do
-  describe yumrepo('newrelic'), :if => os[:family] == 'redhat' do
-    it { is_expected.to exist }
-    it { is_expected.to be_enabled }
-  end
-
-  describe file('/etc/apt/sources.list.d/newrelic.list'), :if => %w[debian ubuntu].include?(os[:family]) do
-    it { is_expected.to be_file }
-  end
-
   describe package 'newrelic-sysmond' do
-    it { is_expected.to be_installed }
+    it { is_expected.to_not be_installed }
   end
 end
