a0f49715cdba6d99cf5da06500255776f7919db6
gdelaro1 <grant.delarosa@kmart.com.au>
2018-04-24 09:14:32 +1000

    added support for amazon linux 2018

--- attributes/repository.rb
+++ attributes/repository.rb
@@ -31,7 +31,7 @@ when 'rhel', 'fedora'
   default['newrelic']['repository']['infrastructure']['uri'] = "https://download.newrelic.com/infrastructure_agent/linux/yum/el/#{rhel_version}/x86_64"
 when 'amazon'
   case node['platform_version'].to_i
-  when 2013, 2014, 2015, 2016, 2017
+  when 2013, 2014, 2015, 2016, 2017, 2018
     rhel_version = 6
   end
   default['newrelic']['repository']['infrastructure']['uri'] = "https://download.newrelic.com/infrastructure_agent/linux/yum/el/#{rhel_version}/x86_64"
