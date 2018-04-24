a70359a62f392f8e3cc06b5c2b14500f62e5bb6f
acondrat <arcadie.condrat@gmail.com>
2018-04-24 11:18:23 +0300

    Adding repository attribute to allow downloading from a custom location (#335)

--- attributes/java_agent.rb
+++ attributes/java_agent.rb
@@ -9,6 +9,7 @@ default['newrelic']['java_agent']['version'] = 'latest'
 default['newrelic']['java_agent']['install_dir'] = '/opt/newrelic/java'
 default['newrelic']['java_agent']['app_user'] = 'newrelic'
 default['newrelic']['java_agent']['app_group'] = 'newrelic'
+default['newrelic']['java_agent']['repository'] = 'https://download.newrelic.com/newrelic/java-agent/newrelic-agent'
 default['newrelic']['java_agent']['audit_mode'] = false
 default['newrelic']['java_agent']['log_file_count'] = 1
 default['newrelic']['java_agent']['log_limit_in_kbytes'] = 0
--- recipes/java_agent.rb
+++ recipes/java_agent.rb
@@ -9,6 +9,7 @@ newrelic_agent_java 'Install' do
   license NewRelic.application_monitoring_license(node)
   version node['newrelic']['java_agent']['version'] unless node['newrelic']['java_agent']['version'].nil?
   install_dir node['newrelic']['java_agent']['install_dir'] unless node['newrelic']['java_agent']['install_dir'].nil?
+  repository node['newrelic']['java_agent']['repository'] unless node['newrelic']['java_agent']['repository'].nil?
   app_location node['newrelic']['java_agent']['app_location'] unless node['newrelic']['java_agent']['app_location'].nil?
   template_cookbook node['newrelic']['java_agent']['template_cookbook'] unless node['newrelic']['java_agent']['template_cookbook'].nil?
   template_source node['newrelic']['java_agent']['template_source'] unless node['newrelic']['java_agent']['template_source'].nil?
