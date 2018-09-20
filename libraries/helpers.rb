effe4dbd641c4239e0de667d92e448284a7424b0
Chris Norman <chrisnorman@gamestop.com>
2018-09-20 16:49:59 -0500

    Added LWRP for New Relic Dot Net Core Agent install
     Changes to be committed:
    	modified:   .kitchen.yml
    	modified:   attributes/repository.rb
    	modified:   libraries/helpers.rb
    	modified:   libraries/matchers.rb
    	modified:   metadata.rb
    	modified:   providers/agent_dotnet.rb
    	new file:   providers/agent_dotnetcore.rb
    	modified:   recipes/default.rb
    	new file:   recipes/dotnetcore_agent.rb
    	new file:   resources/agent_dotnetcore.rb
    	modified:   spec/unit/agent_dotnet_spec.rb
    	new file:   spec/unit/agent_dotnetcore_spec.rb
    	modified:   spec/unit/agent_php_spec.rb
    	modified:   spec/unit/default_spec.rb
    	modified:   spec/unit/repository_spec.rb
    	new file:   test/fixtures/cookbooks/newrelic_lwrp_test/recipes/agent_dotnetcore.rb
    	modified:   test/fixtures/cookbooks/newrelic_lwrp_test/recipes/agent_php.rb
    	new file:   test/integration/dotnetcore-agent/serverspec/default_spec.rb

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -35,9 +35,11 @@ module NewRelic
 
     def install_newrelic_repo_rhel
       yum_repository 'newrelic' do
-        description 'New Relic packages for Enterprise Linux 5 - $basearch'
+        description 'New Relic packages for Enterprise Linux 5'
         baseurl node['newrelic']['repository']['uri']
         gpgkey node['newrelic']['repository']['key']
+        enabled true
+        gpgcheck true
         sslverify node['newrelic']['repository']['ssl_verify']
         proxy node['newrelic']['repository']['proxy'] unless node['newrelic']['repository']['proxy'].nil?
         proxy_username node['newrelic']['repository']['proxy_username'] unless node['newrelic']['repository']['proxy_username'].nil?
