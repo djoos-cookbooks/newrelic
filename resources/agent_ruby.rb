e45ab6eb8caa33d33c68ea261deb3efc391a6e7e
NoMotion <maxssubs@gmail.com>
2019-09-24 01:46:57 -0700

    Trim_stats config item for Java/ruby agent (#378)
    * Add attribute to recipe for distributed tracing
    
    * Add attribute to recipe for Distributed Tracing (ruby)
    
    * Add java/ruby template support for config item trim_stats

--- resources/agent_ruby.rb
+++ resources/agent_ruby.rb
@@ -53,3 +53,4 @@ attribute :browser_monitoring_auto_instrument, :kind_of => String, :default => n
 attribute :cross_application_tracer_enable, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :thread_profiler_enable, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :labels, :kind_of => String, :default => nil
+attribute :trim_stats, :kind_of => [TrueClass, FalseClass], :default => false
